aebabafa523716297fe5e6d169bfd33deabf7dfb
anaissierro <94173575+anaissierro@users.noreply.github.com>
2022-06-03 15:35:16 +0200

    Création du bouton
    réalisation du bouton, il reste à créer le lien vers les ingrédients

--- ImagePopup.js
+++ ImagePopup.js
@@ -1,11 +1,7 @@
 
-// PopUp Flower
 
-
-// Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("FlowerImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -15,23 +11,15 @@ img.onclick = function () {
     captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
 
-
-// PopUp Canette
-
-
-// Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("CanetteImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -41,10 +29,8 @@ img.onclick = function () {
     captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
